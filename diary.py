fb9747373e0c4bbf6314f931bcacd8d6072acc62
Anton <you@example.com>
2026-05-31 13:46:34 +0200

    Проверь сейчас, но с кэшем долго мучался

--- diary.py
+++ diary.py
@@ -1,34 +1,53 @@
-import os 
-if os.path.exists("Diary.txt"):
+cashe_perm = []
+cashe_temp = []
 
-    def add_note():
-        tekst = input("Какую запись вы хотите добавить: ")
-        with open ("Diary.txt", "a", encoding = "utf-8") as file1:
-            file1.write(tekst + "\n")
+
+def add_note(tekst):
+    with open ("Diary.txt", "a", encoding = "utf-8") as file1:
+        file1.write(tekst + "\n")
             
 
-    def read_all():
-        with open ("Diary.txt", "r", encoding = "utf-8") as file1:
-           lines = file1.readlines()
-           for line in lines:
-               print(line.strip())
+def add_all():
+    cashe_temp.clear()
+    with open ("Diary.txt", "r", encoding = "utf-8") as file1:
+        lines = file1.readlines()
+        for line in lines:
+            cashe_temp.append(line.strip())
+
+def update_cache_if_needed():
+    global cashe_perm
+    add_all()
+    if cashe_perm != cashe_temp:
+        cashe_perm = cashe_temp.copy()
+
+
+def show_cashe():
+    for i in cashe_perm:
+        print(i)
+       
         
-    
-    while True:
+
+if __name__ == "__main__":
+   if cashe_perm != cashe_temp:
+       cashe_perm = cashe_temp
+   while True:
         print("\nМеню:")
         print("1. Добавить запись")
         print("2. Показать все записи")
         print("3. Выйти")
 
         choice = input ("Выбери действие :")
-        if choice == "1":
-            add_note()
-        if choice == "2":
-            read_all()
-        if choice == "3":
-            break
+
+        match choice:
+            case "1":
+                note = input("Какую запись вы хотите добавить: ")
+                add_note(note)
+            case "2":
+                update_cache_if_needed()
+                show_cashe()
+            case "3":
+                break
 
         
-            
-else:
-    print ("No file")
+
+          
