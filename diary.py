46b26080594201706ec72c64545032f042d945f1
Anton <you@example.com>
2026-05-31 20:29:22 +0200

    Не буду скрывать, что тут помог чатик

--- diary.py
+++ diary.py
@@ -1,53 +1,52 @@
 cashe_perm = []
-cashe_temp = []
+cache_is_valid = False
 
 
 def add_note(tekst):
-    with open ("Diary.txt", "a", encoding = "utf-8") as file1:
+    with open("Diary.txt", "a", encoding="utf-8") as file1:
         file1.write(tekst + "\n")
-            
 
-def add_all():
-    cashe_temp.clear()
-    with open ("Diary.txt", "r", encoding = "utf-8") as file1:
-        lines = file1.readlines()
-        for line in lines:
-            cashe_temp.append(line.strip())
 
-def update_cache_if_needed():
+def update_cache():
     global cashe_perm
-    add_all()
-    if cashe_perm != cashe_temp:
-        cashe_perm = cashe_temp.copy()
+
+    with open("Diary.txt", "r", encoding="utf-8") as file1:
+        cashe_perm = []
+
+        for line in file1:
+            cashe_perm.append(line.strip())
 
 
 def show_cashe():
     for i in cashe_perm:
         print(i)
-       
-        
+
 
 if __name__ == "__main__":
-   if cashe_perm != cashe_temp:
-       cashe_perm = cashe_temp
-   while True:
+    while True:
         print("\nМеню:")
         print("1. Добавить запись")
         print("2. Показать все записи")
         print("3. Выйти")
 
-        choice = input ("Выбери действие :")
+        choice = input("Выбери действие: ")
 
         match choice:
             case "1":
                 note = input("Какую запись вы хотите добавить: ")
                 add_note(note)
+                cache_is_valid = False
+
             case "2":
-                update_cache_if_needed()
+                if not cache_is_valid:
+                    update_cache()
+                    cache_is_valid = True
+
                 show_cashe()
+
             case "3":
                 break
 
+            case _:
+                print("Такого пункта нет")
         
-
-          
